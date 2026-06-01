b8bf718b045593a31f86e73378dd217ae3ed2d1a
Joe Szymkowicz <joe.szymkowicz@gmail.com>
2026-05-31 21:09:51 -0500

    Switch to chiliaddb and ignore luarc config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game 'gta5'
 
 --[[ Resource Information ]] --
 name 'ox_doorlock'
-version      '1.22.1'
+version '1.22.1'
 license 'GPL-3.0-or-later'
 author 'Overextended'
 repository 'https://github.com/overextended/ox_doorlock'
@@ -23,7 +23,7 @@ client_scripts {
 }
 
 server_scripts {
-    '@oxmysql/lib/MySQL.lua',
+    '@chiliaddb/init.lua',
     'server/main.lua',
 }
 
